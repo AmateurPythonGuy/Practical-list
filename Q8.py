6d98ef439329cffa13b9d152fb69243161861d0b
Wotami <115475156+Gotamantium-Core@users.noreply.github.com>
2023-09-11 22:48:24 +0530

    Better code Q8

--- Q8.py
+++ Q8.py
@@ -1,27 +1,21 @@
-# Read a text file and display the number of vowels/consonants/uppercase/lowercase
-# characters in the file.
-
-try: 
-    file = open(r"sample.txt","r")
-    data = file.read().replace("\n", "")
-
-    up = 0; low = 0
-    vow = 0; cons = 0
-
-    for i in data:
-        if i.lower() in ('a', 'e', 'i', 'o', 'u'):
-            vow += 1
-        else:
-            cons += 1
+# Read a text file and display the number of vowels/consonants/uppercase/lowercase characters in the file.
+try:
+    with open("sample.txt") as file:
+        upper = 0; lower = 0; vows = 0; cons = 0
         
-        if i.isupper():
-            up += 1
-        else:
-            low += 1
+        data = file.read().replace("\n","")
 
-    print(f"[UPPER, lower]: {up, low}")
-    print(f"[vowels, consonants]: {vow, cons}")
-    file.close()
+        for i in data:
+            if i.lower() in ('a', 'e', 'i', 'o', 'u'):
+                vows += 1
+            else:
+                cons += 1
+            
+            if i.isupper():
+                upper += 1
+            else:
+                lower += 1
+        
+        print(f"[Vowels, Consonants, Uppercase, Lowercase] : {[vows, cons, upper, lower]}")
 except FileNotFoundError:
-    print("File not found")
-
+    print("File does not exist")
