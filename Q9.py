6045666aeda434989cb44361c1399a90e9a542f6
Wotami <115475156+Gotamantium-Core@users.noreply.github.com>
2023-09-11 22:52:59 +0530

    better code Q9

--- Q9.py
+++ Q9.py
@@ -1,19 +1,13 @@
-# Write a program to read the content of file line by line and write to another file
-# except for lines containing letter ‘a’.
+# Write a program to read the content of file line by line and write to another file except for lines containing letter ‘a’.
 try:
-    file1 = open(r"sample1.txt","r")
-    file2 = open(r"sample2.txt", "w")
-
-    lines = file1.readlines()
-    final = ""
-
-    for i in lines:
-        if "a" not in i:
-            final += i
-
-    file2.write(final)
-
-    file1.close()
-    file2.close()
+    with open("sample.txt","r") as file1:
+        lines = file1.readlines()
+        
+    with open("sample2.txt","w") as file2:
+        final = ""
+        for i in lines:
+            if "a" not in i:
+                final += i
+        file2.write(final)
 except FileNotFoundError:
-    print("File does not exist")
+    print("File(s) does not exist")
